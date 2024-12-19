754cfe6891e31fbf887a01484e3d7544b455f13c
fathehaharis <fathehaharis69@gmail.com>
2024-12-20 02:32:24 +0800

    update

--- student-panel/studentlogin.php
+++ student-panel/studentlogin.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['password'] = $row['password'];
 
             // Redirect to student dashboard
-            header("Location: dashboard.php");
+            header("Location: studentdashboard.php");
             exit();
         } else {
             $error = "Invalid username or password.";
