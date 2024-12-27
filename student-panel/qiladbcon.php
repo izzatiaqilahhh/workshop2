a0e2d8cdc57341b57f475e918e238a143cb9e283
izzatiaqilahh <izzatiaqilahhh@gmail.com>
2024-12-27 21:47:18 +0800

    Update

--- student-panel/qiladbcon.php
+++ student-panel/qiladbcon.php
@@ -1,10 +1,8 @@
 <?php
 $connection = pg_connect("host=localhost dbname=hrcs user=postgres password=postgresql");
-
 if (!$connection) {
-    echo "An error occurred while connecting to the database.<br>";
+    echo "An error occured.<br>";
     exit;
 }
 
-echo "Connection successful.";
 ?>
