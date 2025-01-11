a05559f6494a74891ab1075980893bffe9f7451e
AINAYUSRILL <ainayusrill2706@gmail.com>
2025-01-11 12:08:56 +0800

    update

---
+++ student-panel/staff-panel/admin/profile-admin.php
@@ -0,0 +1,106 @@
+<?php include('includes/header.php'); ?>
+
+<title>E-Hostel Room Complaint - My Profile</title>
+
+<!-- App Content -->
+<div class="main-content app-content">
+    <div class="container">
+
+        <!-- Page Header -->
+        <div class="d-md-flex d-block align-items-center justify-content-between my-4  page-header-breadcrumb">
+            <h1 class="page-title fw-semibold fs-22 mb-0">Profile</h1>
+            <div class="ms-md-1 ms-0">
+                <nav>
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+
+        <!-- Row 1 -->
+        <div class="row">
+            <div class="card custom-card">
+                <div class="card-header">
+                    <div class="card-title">
+                        My Details
+                    </div>
+                </div>
+                <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label>Staff Name</label>
+                                        <input type="text" name="name" value="" required class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label>Staff Number</label>
+                                        <input type="text" name="staffno" value="" required class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label>Email Address</label>
+                                        <input type="email" name="email" value="" required class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label>Phone Number</label>
+                                        <input type="text" name="phone" value="" required class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label>Position</label>
+                                        <input type="text" name="position" value="" required class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="mb-3">
+                                        <button type="submit" name="editProfile" class="btn btn-primary">Save Changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+         <!-- Row 2 -->
+         <div class="row">
+            <div class="card custom-card">
+                <div class="card-header">
+                    <div class="card-title">
+                        Change Password
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                 <label>Password</label>
+                                <input type="password" name="password" required class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label>Confirm Password</label>
+                                <input type="password" name="cpassword" required class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <button type="submit" name="editProfile1" class="btn btn-primary">Save Changes</button>
+                            </div>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
