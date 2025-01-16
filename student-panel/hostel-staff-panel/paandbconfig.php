1b8e47ffad75a8f84737c1abc9d2996ef1832727
muhd farhan <paanirate@gmail.com>
2025-01-16 02:52:32 +0000

    update code

--- student-panel/hostel-staff-panel/paandbconfig.php
+++ student-panel/hostel-staff-panel/paandbconfig.php
@@ -7,6 +7,24 @@ $username = "root";        // Replace with your MySQL username
 $password = "";            // Replace with your MySQL password
 $dbname = "Workshop2";     // Replace with your database name
 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = "192.168.0.250";
+$user = "paan";
+$pass = "abc123";
+$db = "workshop2";
+
+// Create connection
+$connAisar = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($connAisar->connect_error) {
+    die("Connection failed: " . $connAisar->connect_error);
+}
+
+echo "Connected successfully!<br>";
 // Create connection using MySQLi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -17,4 +35,11 @@ if ($conn->connect_error) {
 
 // Close the connection (optional, for good practice)
 //$conn->close();
+
+
+
+
+
+
+
 ?>  
